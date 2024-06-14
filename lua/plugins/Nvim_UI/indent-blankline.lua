e509aa40eefd086988a6e6a788d2a3e51240effa
Thirdwinter <zzx15601035637@outlook.com>
2024-06-14 14:41:29 +0800

    修改缩进线

--- lua/plugins/Nvim_UI/indent-blankline.lua
+++ lua/plugins/Nvim_UI/indent-blankline.lua
@@ -1,31 +1,33 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--
+-- local highlight = {
+--   "RainbowRed",
+--   "RainbowYellow",
+--   "RainbowBlue",
+--   "RainbowOrange",
+--   "RainbowGreen",
+--   "RainbowViolet",
+--   "RainbowCyan",
+-- }
+--
+-- local hooks = require "ibl.hooks"
+-- -- create the highlight groups in the highlight setup hook, so they are reset
+-- -- every time the colorscheme changes
+-- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+--   vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+--   vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+--   vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+--   vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+--   vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+--
+--   vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+--   vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+-- end)
 
-local highlight = {
-  "RainbowRed",
-  "RainbowYellow",
-  "RainbowBlue",
-  "RainbowOrange",
-  "RainbowGreen",
-  "RainbowViolet",
-  "RainbowCyan",
-}
-
-local hooks = require "ibl.hooks"
--- create the highlight groups in the highlight setup hook, so they are reset
--- every time the colorscheme changes
-hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-  vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-  vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-  vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-  vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-
-  vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-  vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-end)
-
-return {
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
-  -- main = "ibl",
-  require("ibl").setup { indent = { highlight = highlight } },
-}
+return { "lukas-reineke/indent-blankline.nvim", main = "ibl", enable = false }
+-- main = "ibl",
+-- config = function()
+--   require("ibl").setup { --[[ indent = { highlight = highlight }  ]]
+--   }
+-- end,
+-- require("ibl").setup { indent = { highlight = highlight } },
